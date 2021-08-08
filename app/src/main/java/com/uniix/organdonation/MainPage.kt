1b185c927d74a19bf0f918d4527b3724c0d932e6
ManasSardana <manassardana@gmail.com>
2021-08-08 18:23:05 +0530

    r.id.nav

--- app/src/main/java/com/uniix/organdonation/MainPage.kt
+++ app/src/main/java/com/uniix/organdonation/MainPage.kt
@@ -41,7 +41,7 @@ class MainPage : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home
+                R.id.nav_home,R.id.nav_donate,R.id.nav_profile,R.id.nav_about
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)

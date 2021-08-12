20b525ffb90bfafb11a14434546243333168f5b0
varun <prabhujivats@gmail.com>
2021-08-12 11:31:10 +0530

    profile compelete

--- app/src/main/java/com/uniix/organdonation/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/uniix/organdonation/ui/profile/ProfileFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.SpinnerAdapter
 import androidx.fragment.app.Fragment
+import com.uniix.organdonation.R
 import com.uniix.organdonation.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
     //Initializing Variables
     private lateinit var profileFragment: FragmentProfileBinding
+    private var bloodGroupOptions = arrayListOf<Any>("A-", "A+", "B-", "B+", "AB-", "AB+", "O-", "O+")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +23,12 @@ class ProfileFragment : Fragment() {
     ): View? {
 
         profileFragment = FragmentProfileBinding.inflate(inflater)
+
+        val bloodGroupAdapter =
+            context?.let { ArrayAdapter(it, R.layout.spinner_list, bloodGroupOptions) } as SpinnerAdapter
+        //ageDataAdapter.setDropDownViewResource(R.layout.spinner_list)
+        profileFragment.bloodSpinnerProfile.adapter = bloodGroupAdapter
+
         return profileFragment.root
 
     }
